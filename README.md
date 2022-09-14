c952a06fd8f36e3136d70e34d7078f9472953ca4
Jason ONeill <jason.a.oneill@gmail.com>
2022-09-14 11:10:05 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Project2-Gatehouse
+
+NavBar = 6 buttons
+Each button will display given area and hide the others.
+Given area will be in a Form style.
+
---
+++ assests/js/main.js
@@ -0,0 +1,4 @@
+$('.specials').click(function(){
+	$('p').css('color', 'yellow');
+    $('p').text("Mon - Sat: 05:00 - 11:00, Sun: Closed. Bays: 17 - 24. Office: 10 ");
+});
