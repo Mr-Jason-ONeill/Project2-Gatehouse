3d37add9dab05f07879f9329ddeb992c669aea91
Jason ONeill <jason.a.oneill@gmail.com>
2022-09-29 09:50:41 +0000

    changed .text's to .html's

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,47 +1,74 @@
-// Nav buttons 
+
+// ------------------  Nav buttons 
 
 $('.goods-in').click(function(){
-	$('p').css('color', '#2cf915');
-    $('p').text(`Goods In, Mon - Fri: 05:00 - 16:30,  Sat: 05:00 - 14:30, Sun: 05:00 - 11:00. 
-     Ambient: Bays: 7 - 16, Eggs: Bays: 13 - 16, Toilet Roll: Bays: 5 - 6, Leaflets/Posters: Bays: 24 
-     Office: Bay: 10`);
+    let goodsInfo = `
+       Goods In: Mon - Fri: 05:00 - 16:30, Sat: 05:00 - 14:30, Sun: 05:00 - 11:00 <br/>
+       Ambient: Bays 7 - 16, Eggs: Bays: 13 - 16, Toilet Roll: Bays: 5 or 6, Leaflets/Posters: Bay: 24 <br/>
+       Office: Bay: 10`;
+
+    $('#info').css('color', '#2cf915');
+    $('#info').html(goodsInfo);
 });
+
 
 $('.specials').click(function(){
-	$('p').css('color', 'yellow');
-    $('p').text(` Mon - Sat: 05:00 - 11:00, Sun: Closed. 
-    Bays: 17 - 24.  
-    Office: Bay: 10 `);
+    let specialsInfo = `
+       Specials: Mon - Sat: 05:00 - 11:00, Sun: Closed <br/>
+       Specials: Bays: 17 - 24 <br/>
+       Office: Bay: 10`;
+
+    $('#info').css('color', 'yellow');
+    $('#info').html(specialsInfo);
+
 });
 
-$('.tca').click(function(){
-	$('p').css('color', '#04015a');
-    $('p').text(` All Week: 05:00 - 08:00 (Sometimes 09:00 but must call TCA first). 
-    Chilled: Bays: 58 - 65, Frozen: Bays: 66 - 70  
-    Office: Bay: 55 `);
+
+$('.tca').click(function() {
+    let tcaInfo = `
+        TCA: All Week: 05:00 - 08:00 (Sometimes 09:00 call TCA to ask first) <br/>
+        Chilled: Bays: 58 - 65, Frozen: Bays: 66 - 70, Double Deckers: Refer to Double Decker Tab <br/>
+        Office: Bay: 55`;
+
+    $('#info').css('color', '#04015a');
+    $('#info').html(tcaInfo);
+
 });
 
-$('.ptz').click(function(){
-	$('p').css('color', '#99e9ff');
-    $('p').text(` All Week: 13:00 - Onwards *No earlier* 
-    Produce(Potatoes, Bananas, Temp: +4 and up): Bays: 25 - 49, 
-    Chilled(Grapes, Berries, Temp: 0 to +3): Bays: 58 - 62
-    Meat/Fish: Bays: 63 - 70  
-    Office: Bay: 55 `);
+
+$('.ptz').click(function() {
+    let ptzInfo = `
+        PTZ: All Week: 13:00 onwards *No Earlier than 13:00* <br/>
+        Produce: (+4 And Above)(Examples: Potatoes, Bananas), Bays: 25 - 49 <br/>
+        Chilled (0 To +3)(Examples: Grapes, Berries), Bays: 58 - 62 <br/>
+        Chilled(Meat)(Examples: Steak, Fish Fillet), Bays: 63 - 70 <br/>
+        Bread (Goes to Goods In Area), Bays: 19 - 21 <br/>
+        Produce And Chilled Office: Bay 55, Bread Office: Bay 10 `;
+
+    $('#info').css('color', '#99e9ff');
+    $('#info').html(ptzInfo);
+
 });
 
 $('.double-deckers').click(function(){
-	$('p').css('color', '#990000');
-    $('p').text(` *Be sure to write it on their paper-work too*. 
-    TCA & PTZ: Bays: 55 - 57, Goods In & Specials are soft top bays and are fine anywhere. 
-     `);
+    let doubleDeckerInfo = `
+      ** Be sure to write on driver paperwork ** <br/>
+      TCA & PTZ: Bays: 55 - 57 <br/>
+      Goods In Bays are all soft top and are fine with Double Deckers ` ;
+
+    $('#info').css('color', '#990000');
+    $('#info').html(doubleDeckerInfo);
 });
 
-$('.other').click(function(){
-	$('p').css('color', '#660053');
-    $('p').text(` Contractors are usually for VMU(Truck Wash) or Maintenance: Send to correct location, give Contractor card. 
-    Collecting trays or pallets are for Pallet Area.
-    Collecting Trays: Bays: 50 - 54, Office: Bay: 52 (Give PTZ card).   
-    Pallets: Send to Pallet Area, No card needed.
-     `);
-});
+$('.other').click(function() {
+    let otherInfo = `
+        Contractors (Examples: BVS, Intergral, Crown, KIS, Tennant, CimTech, Rentokil, BetterServe, Dale): Will Require Contractor Cards <br/>
+        Delivering Fuel/AdBlue: Send to VMU(Truck Wash), No card needed <br/>
+        Collecting Pallets: Send to Pallet Area, No card needed <br/>
+        Collecting Trays: Bays: 50 - 54, Office: Bay: 52, Will Require PTZ card ` ;
+
+    $('#info').css('color', '#660053');
+    $('#info').html(otherInfo);
+}); 
+
+// ---------------------- Form 
